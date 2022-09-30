c6fd4c062c2aa78cb0d79e6de1d59fe8245a4fb4
Darkindast <109829419+Darkindast@users.noreply.github.com>
2022-09-30 10:05:19 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 double poww(double chislo, int stepen){ 
 
     if (stepen==0){
@@ -20,24 +19,27 @@ double ab(double chislo){
 }
  
 double f2(double x, double e){
-    double x0 = 1;
-    double sum = x0;
+    
+    double p = 1, sum = p, y =x*x;
+
     int i = 1;
-    double y;
-    y = x * x;
- while (ab(x0)>e){
+ while (ab(p)>e){
  
-    x0 = ((x0*poww(-1, i)*y)/i);
+    p = ((p*poww(-1, i)*y)/i);
     i+=1;
-    sum+=x0;
+    sum+=p;
     }
+    
     return sum;
 }
  
 int main(){
+    
     double x, e;
-    scanf("%lf", &e);
+    
     scanf("%lf", &x);
+    scanf("%lf", &e);
     printf("%lf\n",f2(x, e));
+    
     return 0;
 }
